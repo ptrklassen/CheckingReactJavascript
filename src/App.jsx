628b3cb833df7bb466657b10f74b941e68a040ea
Peter Klassen <ptrklassen@gmail.com>
2023-08-08 19:03:05 +0000

    App renders basic Todo list

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
-import './App.css'
+import React from "react";
+import './App.css';
+
 
 export default function App() {
+  const [todos, setTodos] = React.useState([
+  { id: 1, text: "Take a shower", done: false },
+  { id: 2, text: "Go to Costco", done: false },
+  { id: 3, text: "By a wheelbarrow tire", done: false }
+])
+ 
   return (
-    <main>
-      <h1>React ⚛️ + Vite ⚡ + Replit 🌀</h1>
-    </main>
+    <div className="App">
+      <h1>ToDo List</h1>
+
+      <ToDoList todos={todos}/>
+      {/* <AddToDo setTodos={setTodos}/> */}
+    </div>
   )
 }
+
+
+function ToDoList({ todos }) {
+  return (
+    <ul>
+      {todos.map(todo => 
+        <li key={todo.id}>{todo.text}</li>
+        )}
+    </ul>
+  )  
+}
+
+
