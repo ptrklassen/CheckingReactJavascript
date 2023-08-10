319c915f8770e1933a59dec49474b6fe2168c263
Peter Klassen <ptrklassen>
2023-08-10 19:29:50 +0000

    Todos use priority

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import './App.css';
 
 export default function App() {
   const [todos, setTodos] = React.useState([
-  { id: 1, text: "Take a shower", done: true },
-  { id: 2, text: "Go to Costco", done: false },
-  { id: 3, text: "By a wheelbarrow wheel", done: false }
+  { id: 1, text: "Take a shower", done: true, priority: 2 },
+  { id: 2, text: "Go to Costco", done: false, priority: 4 },
+  { id: 3, text: "By a wheelbarrow wheel", done: false, priority: 3 }
 ])
  
   return (
@@ -39,11 +39,39 @@ function ToDoList({ todos, setTodos }) {
       {todos.map((todo) => ( 
         <li 
           onDoubleClick={() => handleToggleTodo(todo)}
-          style={{ textDecoration: todo.done ? "line-through" : "" }} key={todo.id}>{todo.text} <DeleteTodo todo={todo} setTodos={setTodos} /></li>
+          style={{ textDecoration: todo.done ? "line-through" : "" }} key={todo.id}> <PrioritizeTodo todo={todo} setTodos={setTodos} /> {todo.text} <DeleteTodo todo={todo} setTodos={setTodos} /></li>
         ))}
     </ul>
   );
 }
+
+function PrioritizeTodo({ todo, setTodos }) {
+  function handleChangePriority(event) {
+    event.preventDefault();
+    const priorityValue = event.target.value;
+    setTodos((prevTodos) => {
+        return prevTodos.map(todoListItem => 
+      todoListItem.id === todo.id 
+        ? {
+          ...todoListItem, priority: priorityValue
+        } 
+        : todoListItem
+    );
+      })
+  }
+  return (
+    <select name="priority" style={{ marginRight: 5, width: 90 }} value={todo.priority} onChange={ handleChangePriority }>
+      <option value="">Priority</option>
+      <option value="1">Frog</option>
+      <option value="2">Today</option>
+      <option value="3">This Week</option>
+      <option value="4">This Month</option>
+      <option value="5">Future</option>
+    </select>
+  )
+}
+
+
 function DeleteTodo({ todo, setTodos }) {
   function handleDeleteTodo() {
     const confirmed = window.confirm("Do you want to delete this?");
@@ -53,19 +81,18 @@ function DeleteTodo({ todo, setTodos }) {
       })
     }
   }
-  
   return (
     <span 
       onClick={handleDeleteTodo}
       role="button"
       style={{ marginLeft: 10, color: "red", fontWeight: "bold", cursor: "pointer" }}
-      >Del</span>
+      >{"\u274C"}</span>
   )
 }
 
+
 function AddToDo({ setTodos }) {
-const inputRef = React.useRef();
-  
+const inputRef = React.useRef();  
   function handleAddToDo(event) {
     event.preventDefault();
     const text = event.target.elements.addTodo.value;
@@ -79,11 +106,10 @@ const inputRef = React.useRef();
     });
     inputRef.current.value = "";
   }
-  
   return (
     <form onSubmit={ handleAddToDo }>
       <input name="addTodo" placeholder="Add ToDo" ref={ inputRef }/>
-      <button type="submit">Add</button>
+      <button type="submit" style={{marginLeft: 5}}>Add</button>
     </form>
   );
 }
